26a288eb39a66831c8df575f50327d48cdee8c14
mhnuk2007 <128143049+mhnuk2007@users.noreply.github.com>
2025-04-09 02:44:08 +0500

    Update StringContainsExample.java

--- Variables/StringContainsExample.java
+++ Variables/StringContainsExample.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class StringContainsExample {
+    public static void main(String[] args) {
+        String mainString = "Learning Java is fun and powerful!";
+        String keyword1 = "Java";
+        String keyword2 = "Python";
+
+        // Check if mainString contains keyword1
+        if (mainString.contains(keyword1)) {
+            System.out.println("The string contains \"" + keyword1 + "\".");
+        } else {
+            System.out.println("The string does not contain \"" + keyword1 + "\".");
+        }
+
+        // Check if mainString contains keyword2
+        if (mainString.contains(keyword2)) {
+            System.out.println("The string contains \"" + keyword2 + "\".");
+        } else {
+            System.out.println("The string does not contain \"" + keyword2 + "\".");
+        }
+    }
 }
