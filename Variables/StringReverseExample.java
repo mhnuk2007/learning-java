51f194b5b3374e42f45d3fd80307603c7bca9080
mhnuk2007 <128143049+mhnuk2007@users.noreply.github.com>
2025-04-09 02:52:11 +0500

    Update StringReverseExample.java

--- Variables/StringReverseExample.java
+++ Variables/StringReverseExample.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class StringReverseExample {
+    public static void main(String[] args) {
+        String originalString = "Java Programming";
+
+        // Using StringBuilder's reverse() method
+        String reversedString = new StringBuilder(originalString).reverse().toString();
+
+        // Output the reversed string
+        System.out.println("Reversed string: " + reversedString);
+    }
 }
+
