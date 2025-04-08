ce047dc3dacc15e94925cc1d571d5bf86fcdbbee
mhnuk2007 <128143049+mhnuk2007@users.noreply.github.com>
2025-04-09 02:40:14 +0500

    Update StringConcatenationExample.java

--- Variables/StringConcatenationExample.java
+++ Variables/StringConcatenationExample.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class StringConcatenationExample {
+    public static void main(String[] args) {
+        String firstName = "Mohan";
+        String lastName = "Lal";
+
+        // 1. Using + operator
+        String fullName1 = firstName + " " + lastName;
+        System.out.println("Using + operator: " + fullName1);
+
+        // 2. Using concat() method
+        String fullName2 = firstName.concat(" ").concat(lastName);
+        System.out.println("Using concat(): " + fullName2);
+
+        // 3. Using StringBuilder (efficient in loops)
+        StringBuilder sb = new StringBuilder();
+        sb.append(firstName);
+        sb.append(" ");
+        sb.append(lastName);
+        System.out.println("Using StringBuilder: " + sb.toString());
+
+        // 4. Using String.format()
+        String fullName4 = String.format("%s %s", firstName, lastName);
+        System.out.println("Using String.format(): " + fullName4);
+    }
 }
