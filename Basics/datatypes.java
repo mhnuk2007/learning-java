191d809d76572c61e2ab52f5e1f39c7517951577
Mohan Lal <mhnuk2007@gmail.com>
2026-02-28 20:31:05 +0500

    update datatypes

--- Basics/datatypes.java
+++ Basics/datatypes.java
@@ -2,13 +2,13 @@ package Basics;
 
 public class datatypes {
     public static void main(String[] args) {
-        byte num = 5;
+        byte num = 127;
         int num1  = 5;
         double num2 = 5.3;
         float num3 = 5.3f;
         boolean bool = true;
         char ch = 'a';
-        System.out.println(num);
+        System.out.println(((Object)(num+1)).getClass().getSimpleName());
         System.out.println(num1);
         System.out.println(num2);
         System.out.println(num3);
