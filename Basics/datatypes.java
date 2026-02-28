bedd549ee161bb78810c257130bfbc6d80014e85
Mohan Lal <mhnuk2007@gmail.com>
2026-02-28 20:20:31 +0500

    Add datatypes

---
+++ Basics/datatypes.java
@@ -0,0 +1,21 @@
+package Basics;
+
+public class datatypes {
+    public static void main(String[] args) {
+        byte num = 5;
+        int num1  = 5;
+        double num2 = 5.3;
+        float num3 = 5.3f;
+        boolean bool = true;
+        char ch = 'a';
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(bool);
+        System.out.println(ch);
+
+
+
+    }
+}
