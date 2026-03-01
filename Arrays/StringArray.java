92be55d22d558a6e100179e28cfaa4ca0c0f262d
Mohan Lal <mhnuk2007@gmail.com>
2026-03-01 17:44:42 +0500

    update string array examples

--- Arrays/StringArray.java
+++ Arrays/StringArray.java
@@ -1,10 +1,21 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class StringArray {
     public static void main(String[] args) {
         String[] strArr = new String[5];
         for (int i = 0; i < strArr.length; i++) {
             System.out.println(strArr[i]);
         }
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < strArr.length; i++) {
+            System.out.println("enter a string: ");
+            strArr[i] = sc.next();
+        }
+
+        System.out.println(Arrays.toString(strArr));
     }
 }
